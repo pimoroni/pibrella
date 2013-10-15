6d3adc3409c7edb9be32f6265cedcf212412ea10
Gee Bartlett <geeus81@gmail.com>
2013-10-15 15:18:36 +0100

    Update outputs.py

--- outputs.py
+++ outputs.py
@@ -5,8 +5,22 @@ import pibrella
 
 pib = pibrella.pibrella()
 
+sleeptime = 0.2
+
 while True:
   pib.output22(1)
-  sleep(5)
+  sleep(sleeptime)
   pib.output22(0)
-  sleep(5)
+  sleep(sleeptime)
+  pib.output23(1)
+  sleep(sleeptime)
+  pib.output23(0)
+  sleep(sleeptime)
+  pib.output24(1)
+  sleep(sleeptime)
+  pib.output24(0)
+  sleep(sleeptime)
+  pib.output25(1)
+  sleep(sleeptime)
+  pib.output25(0)
+  sleep(sleeptime)
