6b20842a62403276326399c082026c63f9f0e8df
Philip Howard <phil@gadgetoid.com>
2014-02-25 08:30:22 +0000

    Updated Pibrella library

--- pibrella.py
+++ pibrella.py
@@ -1,209 +1,626 @@
+import sys, time, threading, thread, signal, atexit
 import RPi.GPIO as GPIO
-from time import sleep
 
+# Pibrella Pins, these are BCM
+PB_PIN_LIGHT_RED   = 27 # 21 on Rev 1
+PB_PIN_LIGHT_AMBER = 17
+PB_PIN_LIGHT_GREEN = 4
+
+# Inputs
+PB_PIN_INPUT_A = 9
+PB_PIN_INPUT_B = 7
+PB_PIN_INPUT_C = 8
+PB_PIN_INPUT_D = 10
+
+# Outputs
+PB_PIN_OUTPUT_A = 22
+PB_PIN_OUTPUT_B = 23
+PB_PIN_OUTPUT_C = 24
+PB_PIN_OUTPUT_D = 25
+
+# Onboard button
+PB_PIN_BUTTON = 11
+
+# Onboard buzzer
+PB_PIN_BUZZER = 18
+
+# Number of times to udpate
+# pulsing LEDs per second
+PULSE_FPS = 50
+PULSE_FREQUENCY = 100
+
+DEBOUNCE_TIME = 300
+
+# Basic wrapper around Thread to add Event
+# for stopping the execution loop and
+# exiting cleanly.
+class StoppableThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.stop_event = threading.Event()
+		self.daemon = True         
+
+	def start(self):
+		if self.isAlive() == False:
+			self.stop_event.clear()
+			threading.Thread.start(self)
+
+	def stop(self):
+		if self.isAlive() == True:
+			# set event to signal thread to terminate
+			self.stop_event.set()
+			# block calling thread until thread really has terminated
+			self.join()
+
+# Basic thread wrapper class for running functions
+# asyncronously. Return False from your function
+# to abort looping.
+class AsyncWorker(StoppableThread):
+	def __init__(self, todo):
+		StoppableThread.__init__(self)
+		self.todo = todo
+
+	def run(self):
+		while self.stop_event.is_set() == False:
+			if self.todo() == False:
+				break
+
+
+class Pulse(StoppableThread):
+	def __init__(self,pin,time_on,time_off,transition_on,transition_off):
+		StoppableThread.__init__(self)
+
+		self.pin = pin
+		self.time_on = (time_on)
+		self.time_off = (time_off)
+		self.transition_on = (transition_on)
+		self.transition_off = (transition_off)
+
+		self.fps = PULSE_FPS
+
+		# Total time of transition
+		self.time_start = time.time()
+
+	def start(self):
+		self.time_start = time.time()
+		StoppableThread.start(self)
+
+	def run(self):
+		# This loop runs at the specified "FPS" uses time.time() 
+		while self.stop_event.is_set() == False:
+			current_time = time.time() - self.time_start
+			delta = current_time % (self.transition_on+self.time_on+self.transition_off+self.time_off)
+
+			if( delta <= self.transition_on ):
+				# Transition On Phase
+				self.pin.duty_cycle( round(( 100.0 / self.transition_on ) * delta) )
+
+			elif( delta > self.transition_on + self.time_on and delta <= self.transition_on + self.time_on + self.transition_off ):
+				# Transition Off Phase
+				current_delta = delta - self.transition_on - self.time_on
+				self.pin.duty_cycle( round(100.0 - ( ( 100.0 / self.transition_off ) * current_delta )) )
+
+			elif( delta > self.transition_on and delta <= self.transition_on + self.time_on ):
+				self.pin.duty_cycle( 100 )
+
+			elif( delta > self.transition_on + self.time_on + self.transition_off ):
+				self.pin.duty_cycle( 0 )
+
+			time.sleep(1.0/self.fps) # Pulse framerate
+
+		self.pin.duty_cycle( 0 )
+
+	# This alternative uses tighter loops, but
+	# any interruption can cause simultaneously started
+	# pulses to desyncronise quite obviously
+	def dont_run(self):
+		while self.stop_event.is_set() == False:
+			if self.transition_on > 0:
+				for x in xrange(0, 100):
+					if self.stop_event.is_set():
+						break
+					self.pin.duty_cycle(x)
+					time.sleep(float(self.transition_on)/100)
+			else:
+				self.pin.duty_cycle(100)
+
+			for x in xrange(0, 1000):
+				time.sleep(self.time_on/1000)
+				if self.stop_event.is_set():
+					break
+
+			if self.transition_off > 0:
+				for x in reversed(xrange(0, 100)):
+					if self.stop_event.is_set():
+						break
+					self.pin.duty_cycle(x)
+					time.sleep(float(self.transition_off)/100)
+			else:
+				self.pin.duty_cycle(0)
+
+			for x in xrange(0, 1000):
+				time.sleep(self.time_off/1000)
+				if self.stop_event.is_set():
+					break
+
+# Provies an "all" method to Pins collections
+class PinIterator:
+
+	def __init__(self):
+		self._all = []
+
+	# Support accessing with []
+	def __getitem__(self, key):
+		return self._all[key]
+
+	def count(self):
+		return len(self._all)
+
+	def _add(self,obj):
+		# Handle adding additional items after init
+		self._all.append(obj)
+
+	# Catch any calls to self.method_name and
+	# hand them off to each class instance
+	def __getattr__(self,name):
+		def handlerFunction(*args,**kwargs):
+			_results = []
+			for node in self._all:
+				_results.append( getattr(node,name)(*args) )
+
+			# Return the array of results
+			return _results
+
+		return handlerFunction
+
+class Pins:
+
+	def __init__(self, **kwargs):
+		self.all = PinIterator()
+		self._add(**kwargs)
+
+	# Support accessing with [n]
+	def __getitem__(self, key):
+		return self.all[key]
+
+	def count(self):
+		return self.all.count()
+
+	def _add(self, **kwargs):
+		for name in kwargs:
+
+			# Make the light available as an attribute of the pins instance
+			setattr(self, name, kwargs[name])
+
+			# Add the current light to the iterator for "all"
+			self.all._add(kwargs[name])
+
+# Pin contains methods that apply
+# to both inputs and outputs
+class Pin(object):
+	type = 'Pin'
+
+	def __init__(self, pin):
+		self.pin = pin
+		self.last = self.read()
+		self.handle_change = False
+		self.handle_high = False
+		self.handle_low = False
+
+	def has_changed(self):
+		if self.read() != self.last:
+			self.last = self.read()
+			return True
+		return False
+
+	def is_off(self):
+		return self.read() == 0
+
+	def is_on(self):
+		return self.read() == 1
+
+	def read(self):
+		return GPIO.input(self.pin)
+
+	def stop(self):
+		return True
+
+	is_high = is_on
+	is_low = is_off
+	get = read
+
+# Input contains methods that
+# apply only to inputs
+class Input(Pin):
+
+	type = 'Input'
+
+	def __init__(self, pin):
+		if self.type == 'Button':
+			GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		else:
+			GPIO.setup(pin, GPIO.IN)
+		super(Input,self).__init__(pin)
+
+	def on_high(self, callback, bouncetime=DEBOUNCE_TIME):
+		def handle_callback(pin):
+			callback(self)
+		GPIO.add_event_detect(self.pin, GPIO.RISING, callback=handle_callback, bouncetime=bouncetime)
+
+	def on_low(self, callback, bouncetime=DEBOUNCE_TIME):
+		def handle_callback(pin):
+			callback(self)
+		GPIO.add_event_detect(self.pin, GPIO.RISING, callback=handle_callback, bouncetime=bouncetime)
+		
+	def on_changed(self, callback, bouncetime=DEBOUNCE_TIME):
+		def handle_callback(pin):
+			callback(self)
+		GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=handle_callback, bouncetime=bouncetime)
+
+	def clear_events(self):
+		GPIO.remove_event_detect(self.pin)
+
+	# Alias handlers
+	changed = on_changed
+	pressed = on_high
+	released = on_low
+
+class Button(Input):
+
+	type = 'Button'
+
+	def __init__(self, pin):
+		super(Button,self).__init__(pin)
+
+	def is_pressed(self):
+		return self.is_on()
+
+	def is_released(self):
+		return self.is_off()
+
+# Output contains methods that
+# apply only to outputs
+class Output(Pin):
+
+	type = 'Output'
+
+	def __init__(self, pin):
+		#print "Setting " + str(pin) + " to OUTPUT"
+		GPIO.setup(pin, GPIO.OUT, initial=0)
+		super(Output,self).__init__(pin)
+		self.gpio_pwm = GPIO.PWM(pin,1)
+
+	# The crux of an Output is its write function
+
+	def pwm(self,freq,duty_cycle = 50):
+		self.gpio_pwm.ChangeDutyCycle(duty_cycle)
+		self.gpio_pwm.ChangeFrequency(freq)
+		self.gpio_pwm.start(duty_cycle)
+
+	def frequency(self,freq):
+		self.gpio_pwm.ChangeFrequency(freq)
+
+	def duty_cycle(self,duty_cycle):
+		self.gpio_pwm.ChangeDutyCycle(duty_cycle)
+
+	def stop(self):
+		self.gpio_pwm.stop()
+
+	def write(self,value):
+		GPIO.output(self.pin,value)
+
+	def on(self):
+		self.duty_cycle(100)
+		self.gpio_pwm.stop()
+		self.write(1)
+
+	def off(self):
+		self.duty_cycle(0)
+		self.gpio_pwm.stop()
+		self.write(0)
+
+	# Alias on/off to conventional names
+	high = on
+	low  = off
+
+	def toggle(self):
+		if( self.read() == 1 ):
+			self.write(0)
+		else:
+			self.write(1)
+
+class Light(Output):
+
+	type = 'Light'
+
+	def __init__(self,pin):
+		super(Light,self).__init__(pin)
+		self.pulser = Pulse(self,0,0,0,0)
+		self.blinking = False
+		self.pulsing = False
+
+	def blink(self,on=1,off=-1):
+		if off == -1:
+			off = on
+
+		off = float(off)
+		on = float(on)
+
+		total = off + on
+
+		duty_cycle = 100.0 * (on/total)
+
+		# Stop the thread that's pulsing the LED
+		if self.pulsing:
+			self.stop_pulse();
+
+		# Use pure PWM blinking, because threads are fugly
+		if self.blinking:
+			self.frequency(1.0/total)
+			self.duty_cycle(duty_cycle)
+		else:
+			self.pwm(1.0/total,duty_cycle)
+			self.blinking = True
+
+	def pulse(self,time_on=-1,time_off=-1,transition_on=-1,transition_off=-1):
+		# This needs a thread to handle the fade in and out
+
+		if time_off == -1:
+			time_off = time_on
+
+		if transition_off == -1 and transition_on > -1:
+			transition_off = transition_on
+
+		if transition_on == -1:
+			transition_on = time_on
+			transition_off = time_on
+
+		# Fire up PWM if it's not running
+		if self.blinking == False:
+			self.pwm(PULSE_FREQUENCY,0.0)
+
+		# pulse(x,y,0,0) is basically just a regular blink
+		# only fire up a thread if we really need it
+		if transition_on == 0 and transition_off == 0:
+			self.blink(time_on,time_off)
+		else:
+			self.pulser.time_on = time_on
+			self.pulser.time_off = time_off
+			self.pulser.transition_on = transition_on
+			self.pulser.transition_off = transition_off
+			self.pulser.start() # Kick off the pulse thread
+			self.pulsing = True
+
+		self.blinking = True
+
+	def on(self):
+		# Some gymnastics here to fix a big ( in RPi.GPIO?)
+		# That occurs when trying to output(1) immediately
+		# after stopping the PWM
+		blinking = self.blinking
+		self.stop()
+		# A small delay is needed. Ugly, but it works
+		if blinking:
+			time.sleep(0.05)
+		return super(Light,self).on()
+	high = on
+
+	def off(self):
+		# Obviously stop blinking and/or pulsing if we're
+		# turning this light off
+		self.stop()
+		return super(Light,self).off()
+	low = off
+
+	def stop_pulse(self):
+		self.pulsing = False
+		self.pulser.stop()
+		self.pulser = Pulse(self,0,0,0,0)
+
+	def stop(self):
+		self.blinking = False
+		self.stop_pulse()
+
+		# Abruptly stopping PWM is a bad idea
+		# unless we're writing a 1 or 0
+		# So don't inherit the parent classes
+		# stop() since weird bugs happen
+
+		# Threaded PWM access was aborting with
+		# no errors when stop coincided with a
+		# duty cycle change.
+
+		#time.sleep(0.001)
+		#super(Light,self).stop()
+
+class Buzzer(Output):
+
+	type = 'Buzzer'
+
+	def buzz(self,frequency):
+		self.pwm(frequency,30)
+
+	# Play a single note, mathmatically
+	# deduced from its index, offset from 440Hz
+	def note(self,note):
+		note = float(note)
+		a = pow(2.0, 1.0/12.0)
+		self.buzz(440.00 * pow(a,note))
+
+	# Example sound effects
+	def success(self):
+		def success():
+			for note in xrange(0,4):
+				self.note(note)
+				time.sleep(0.2)
+			time.sleep(0.4)
+			self.stop()
+			# Prevent looping
+			return False
+		async = AsyncWorker(success)
+		async.start()
+
+	def fail(self):
+		def fail():
+			for note in reversed(xrange(0,5)):
+				self.note(note)
+				time.sleep(0.2)
+			time.sleep(0.4)
+			self.stop()
+			# Prevent looping
+			return False
+		async = AsyncWorker(fail)
+		async.start()
+
+class Pibrella:
+	light = None
+	input = None
+	output = None
+	button = None
+	buzzer = None
+	pin = None
+	running = False
+	workers = {}
+
+	def async_start(self,name,function):
+		self.workers[name] = AsyncWorker(function)
+		self.workers[name].start()
+
+	def async_stop(self,name):
+		self.workers[name].stop()
+
+	def async_stop_all(self):
+		for worker in self.workers:
+			print "Stopping user task: " + worker
+			self.workers[worker].stop()
+
+	def set_timeout(self,function,seconds):
+		def fn_timeout():
+			time.sleep(seconds)
+			function()
+			return False
+		timeout = AsyncWorker(fn_timeout)
+		timeout.start()
+
+	# Should this ever be exposed to the user?
+	def pause(self):
+		signal.pause()
+
+	# Register a loop to run
+	def loop(self, callback):
+		self.running = True
+		while self.running:
+			callback()
+
+	# Stop a running loop
+	def stop(self):
+		self.running = False
+
+	# Exit cleanly
+	def exit(self):
+		print "\nPibrella exiting cleanly, please wait..."
+
+		print "Stopping flashy things..."
+		self.pin.all.stop()
+
+		print "Stopping user tasks..."
+		self.async_stop_all()
+
+		print "Cleaning up..."
+		GPIO.cleanup()
+
+		print "Goodbye!"
+
+# Set mode to use BCM pin numberings
+# TODO: Probably want to change this to board?
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+#GPIO.setwarnings(False)
 
-GPIO.setup(4, GPIO.OUT) # Red led
-GPIO.setup(17, GPIO.OUT) # Yellow led
-if GPIO.RPI_REVISION == 1:
-  GPIO.setup(21, GPIO.OUT) # Green for Rev 1 boards
-else:
-  GPIO.setup(27, GPIO.OUT) # Green for Rev 2 boards
+# Catch exit and make sure we clean up
+# before classes are too destroyed to do so
+atexit.register(lambda: pibrella.exit())
 
-GPIO.setup(11, GPIO.IN) # Button
+# Instance our Pibrella class
+pibrella = Pibrella()
 
+# Create an object containing our lights
+pibrella.light = Pins()
+pibrella.light._add(red = Light(PB_PIN_LIGHT_RED))
+pibrella.light._add(amber = Light(PB_PIN_LIGHT_AMBER))
+pibrella.light._add(green = Light(PB_PIN_LIGHT_GREEN))
 
-GPIO.setup(10, GPIO.IN) # Inputs
-GPIO.setup(9, GPIO.IN)
-GPIO.setup(8, GPIO.IN)
-GPIO.setup(7, GPIO.IN)
+# Create an object containing our inputs
+pibrella.input = Pins()
+pibrella.input._add(a = Input(PB_PIN_INPUT_A))
+pibrella.input._add(b = Input(PB_PIN_INPUT_B))
+pibrella.input._add(c = Input(PB_PIN_INPUT_C))
+pibrella.input._add(d = Input(PB_PIN_INPUT_D))
 
-GPIO.setup(25, GPIO.OUT) # Outputs
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
+# Create an object contianing our outputs
+pibrella.output = Pins()
+pibrella.output._add(e = Output(PB_PIN_OUTPUT_A))
+pibrella.output._add(f = Output(PB_PIN_OUTPUT_B))
+pibrella.output._add(g = Output(PB_PIN_OUTPUT_C))
+pibrella.output._add(h = Output(PB_PIN_OUTPUT_D))
 
+# And our button
+pibrella.button = pibrella.input.button
 
+# And our buzzer
+pibrella.buzzer = Buzzer(PB_PIN_BUZZER)
 
+# And collect everything into one place
+pibrella.pin = Pins()
 
-class pibrella:
+# Outputs
+pibrella.pin._add(out_e = pibrella.output.e)
+pibrella.pin._add(out_f = pibrella.output.f)
+pibrella.pin._add(out_g = pibrella.output.g)
+pibrella.pin._add(out_h = pibrella.output.h)
 
-  def __init__(self):
-    self.enablepwm = 0
+# Inputs
+pibrella.pin._add(in_a  = pibrella.input.a)
+pibrella.pin._add(in_b  = pibrella.input.b)
+pibrella.pin._add(in_c  = pibrella.input.c)
+pibrella.pin._add(in_d  = pibrella.input.d)
 
-  def enableTGRT(self,value):
-    GPIO.setup(value, GPIO.IN)
+# Lights
+pibrella.pin._add(red   = pibrella.light.red)
+pibrella.pin._add(amber = pibrella.light.amber)
+pibrella.pin._add(green = pibrella.light.green)
 
-  def detectTGRT(self,value):
-    if(GPIO.input(value) == 1):
-      return 1
-    else:
-      return 0
-	 
+# Button
+pibrella.pin._add(button = pibrella.button)
 
-  def enablePWM(self):
-    self.PWM25 = GPIO.PWM(25, 100)
-    self.PWM25.start(0)
-    self.PWM24 = GPIO.PWM(24, 100)
-    self.PWM24.start(0)
-    self.PWM23 = GPIO.PWM(23, 100)
-    self.PWM23.start(0)
-    self.PWM22 = GPIO.PWM(22, 100)
-    self.PWM22.start(0)
-    self.enablepwm = 1
+# Fire up our worker
+# pibrellaworker._setup(pibrella)
 
-  def enableADC(self):
-    print "Enabling ADC"
-    GPIO.setup(10, GPIO.OUT) # MOSI
-    GPIO.setup(9, GPIO.IN) # MISO
-    GPIO.setup(8, GPIO.OUT) # CLK
-    GPIO.setup(7, GPIO.OUT) # CS
+# Alias all the things!
+# This lets users "import pibrella" instead of "import pibrella from pibrella"
+pause = pibrella.pause
 
+# Lets you register a function to loop
+loop = pibrella.loop
 
+# Stops the loop
+stop = pibrella.stop
 
-  def red(self, value):
-    if value == "on" or value == 1:
-      GPIO.output(4, GPIO.HIGH)
-    else:
-      GPIO.output(4, GPIO.LOW)
+# IO
+light = pibrella.light
+input = pibrella.input
+output = pibrella.output
+button = pibrella.button
+buzzer = pibrella.buzzer
+pin = pibrella.pin
 
-  def yellow(self, value):
-    if value == "on" or value == 1:
-      GPIO.output(17, GPIO.HIGH)
-    else:
-      GPIO.output(17, GPIO.LOW)
+# Aliases of input/output and light
+In = IN = input
+Out = OUT = output
+Led = LED = light
 
-  def green(self, value):
-    if value == "on" or value == 1:
-      GPIO.output(27, GPIO.HIGH)
-    else:
-      GPIO.output(27, GPIO.LOW)
+# Advanced: Asyncronous function execution
+async_start = pibrella.async_start
+async_stop = pibrella.async_stop
+async_stop_all = pibrella.async_stop_all
 
-
-  def button(self):
-    if GPIO.input(11):
-      return 1
-    else:
-      return 0
-
-
-  def input10(self):
-    if GPIO.input(10):
-      return 1
-    else:
-      return 0
-
-  def input9(self):
-    if GPIO.input(9):
-      return 1
-    else:
-      return 0
-
-  def input8(self):
-    if GPIO.input(8):
-      return 1
-    else:
-      return 0
-
-  def input7(self):
-    if GPIO.input(7):
-      return 1
-    else:
-      return 0
-
-
-  def output25(self, value):
-    if self.enablepwm == 1:
-      if value == "on":
-        self.PWM25.ChangeDutyCycle(100)
-      else:
-        self.PWM25.ChangeDutyCycle(value)
-    else:
-      if value == "on" or value == 1:
-        GPIO.output(25, GPIO.HIGH)
-      else:
-        GPIO.output(25, GPIO.LOW)
-
-  def output24(self, value):
-    if self.enablepwm == 1:
-      if value == "on":
-        self.PWM24.ChangeDutyCycle(100)
-      else:
-        self.PWM24.ChangeDutyCycle(value)
-    else:
-      if value == "on" or value == 1:
-        if value == "on" or value == 1:
-          GPIO.output(24, GPIO.HIGH)
-        else:
-          GPIO.output(24, GPIO.LOW)
-
-  def output23(self, value):
-    if self.enablepwm == 1:
-      if value == "on":
-        self.PWM23.ChangeDutyCycle(100)
-      else:
-        self.PWM23.ChangeDutyCycle(value)
-    else:
-      if value == "on" or value == 1:
-        if value == "on" or value == 1:
-          GPIO.output(23, GPIO.HIGH)
-        else:
-          GPIO.output(23, GPIO.LOW)
-
-  def output22(self, value):
-    if self.enablepwm == 1:
-      if value == "on":
-        self.PWM22.ChangeDutyCycle(100)
-      else:
-        self.PWM22.ChangeDutyCycle(value)
-    else:
-      if value == "on" or value == 1:
-        if value == "on" or value == 1:
-          GPIO.output(22, GPIO.HIGH)
-        else:
-          GPIO.output(22, GPIO.LOW)
-
-
-
-  def readADC(adcnum, clockpin=8, mosipin=10, misopin=9, cspin=7):
-# borrowed from Adafruit :)
-    if ((adcnum > 7) or (adcnum < 0)):
-      return -1
-    GPIO.output(cspin, True)
- 
-    GPIO.output(clockpin, False) # start clock low
-    GPIO.output(cspin, False) # bring CS low
- 
-    commandout = adcnum
-    commandout |= 0x18 # start bit + single-ended bit
-    commandout <<= 3 # we only need to send 5 bits here
-    for i in range(5):
-      if (commandout & 0x80):
-        GPIO.output(mosipin, True)
-      else:
-        GPIO.output(mosipin, False)
-      commandout <<= 1
-      GPIO.output(clockpin, True)
-      GPIO.output(clockpin, False)
- 
-    adcout = 0
-# read in one empty bit, one null bit and 10 ADC bits
-    
-    for i in range(12):
-      GPIO.output(clockpin, True)
-      GPIO.output(clockpin, False)
-      adcout <<= 1
-      if (GPIO.input(misopin)):
-        adcout |= 0x1
- 
-    GPIO.output(cspin, True)
-    adcout >>= 1 # first bit is 'null' so drop it
-    return adcout
-
-
-
-
-  def cleanup(self):
-    GPIO.cleanup()
-
+set_timeout = pibrella.set_timeout
---
+++ siren.py
@@ -0,0 +1,55 @@
+import pibrella, time, signal
+
+# Pulse all of the lights
+
+TIME_ON = 0.2
+TIME_OFF = 0.2
+FADE_ON = 0.2
+FADE_OFF = 0.2
+
+# Play a siren
+
+siren_stopped = True
+
+def siren():
+	if siren_stopped == True:
+		pibrella.buzzer.stop()
+		return True
+	for x in xrange(-30,30,2):
+		pibrella.buzzer.note(x)
+		time.sleep(0.01)
+	for x in reversed(xrange(-30,30,2)):
+		pibrella.buzzer.note(x)
+		time.sleep(0.01)
+
+
+pibrella.async_start('siren',siren)
+
+def start_siren():
+	global siren_stopped
+	siren_stopped = False
+	pibrella.light.all.pulse(TIME_ON, TIME_OFF, FADE_ON, FADE_OFF)
+
+def stop_siren():
+	global siren_stopped
+	siren_stopped = True
+	pibrella.light.all.stop()
+
+def handle_button(button):
+	global siren_stopped
+	if siren_stopped == True:
+		start_siren()
+		print "Starting Siren"
+	else:
+		stop_siren()
+		print "Stopping Siren"
+
+pibrella.button.released(handle_button)
+
+# Sleep for 20 seconds
+
+#time.sleep(20)
+
+signal.pause()
+
+# Exit!
