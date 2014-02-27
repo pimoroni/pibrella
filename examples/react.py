b061d7e90844af56114955453fe1cd8ebcfdfe84
Philip Howard <phil@gadgetoid.com>
2014-02-27 23:30:25 +0000

    Improvements to react.py

--- examples/react.py
+++ examples/react.py
@@ -4,7 +4,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import pibrella, time, random
 
 # Number of attempts we get before game over
-attempts = 10
+attempts = 4
 
 # Keep track of the score
 score = 0
@@ -20,23 +20,31 @@ button_pressed = False
 
 def handle_reaction(pin):
 	global attempts, current_light, red_time, button_pressed, score
+
+	# Don't handle subsequent button presses
+	# that would be a little harsh!
+	if button_pressed == True:
+		return false
+
 	if current_light == 'red':
 		# Determine how long the reaction took
 		speed = time.time() - red_time
 	
 		# Update the score
+		# Anything slower than 2s
+		# is a penalty, sorry!
 		score = score + (2 - speed)
 		attempts = attempts - 1
 
 		# Feed back to user
 		pibrella.buzzer.success()
-		print("Yay, red light!")
+		
 		print("You took: " + str(speed) + "sec!")
 		print("Score: " + str(score))		
 
 		# Break the red light loop
 		button_pressed = True
-	else:
+	elif current_light == 'amber':
 		# Whoops, the light isn't red yet1
 		pibrella.buzzer.fail()
 		print("Woah, woah, too soon!")
@@ -44,19 +52,29 @@ def handle_reaction(pin):
 		# Update the score with
 		# a time penalty
 		score = score - 0.5
+	
+		# Count it as an attempt
+		attempts = attempts - 1
 		print("Score: " + str(score))
 
+		# Skip to next green
+		button_pressed = True
+
 # Set up the button pressed event with a 20ms debounce
-pibrella.button.pressed(handle_reaction,20)
+pibrella.button.pressed(handle_reaction,30)
 
 # Our main loop, goes from green to amber to red
 # at psuedo random intervals
 def random_lights():
 	global attempts, current_light, red_time, button_pressed, score
 
-	if attempts == 0:
+	if attempts <= 0:
 		print("Game over!")
 		print("You scored: " + str(score))
+
+		# Wait for sounds to finish
+		pibrella.light.pulse()
+		time.sleep(2)
 		exit()
 
 	# Make sure we clear the button_pressed state
@@ -89,8 +107,11 @@ def random_lights():
 	print("Light going red!")
 
 	current_light = 'red'
-	red_time = time.time()
 
+	# I felt it was harsh on fast
+	# reaction times
+	time.sleep(0.1)
+	red_time = time.time()
 	pibrella.light.red.on()
 	pibrella.light.amber.off()
 
