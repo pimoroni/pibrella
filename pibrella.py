ceb598970c96bca79f84830e8a3feed505265193
Philip Howard <phil@gadgetoid.com>
2014-03-05 19:16:30 +0000

    New function pibrella.buzzer.notes with A{octave}:{duration} text notation

--- pibrella.py
+++ pibrella.py
@@ -546,6 +546,11 @@ class Buzzer(Output):
 	def melody(self,notes,duration = 0.5,loop = True):
 		self.stop()
 		time_start = time.time()
+		is_notation = False
+
+		if notes[0] == 'N':
+			is_notation = True
+			notes.pop(0)
 
 		if duration <= 0.0001:
 			raise ValueError('Duration must be greater than 0.0001')
@@ -558,7 +563,7 @@ class Buzzer(Output):
 		total = len(notes) * duration
 
 		def melody():
-			
+
 			now = time.time() - time_start
 			
 			# Play only once if loop is false
@@ -573,11 +578,20 @@ class Buzzer(Output):
 			# Select the note from the notes array
 			note = notes[int(delta)-1]
 			
-			if note == '-':
-				super(Buzzer,self).stop()
+			
+			if is_notation:
+				# this note and above would be OVER NINE THOUSAND Hz!
+				# Treat it as an explicit pitch instead
+				if note == 0:
+					super(Buzzer,self).stop()
+				else:
+					pibrella.buzzer.buzz(note)
 			else:
-				# Play the note
-				pibrella.buzzer.note(note)
+				if note == '-':
+					super(Buzzer,self).stop()
+				else:
+					# Play the note
+					pibrella.buzzer.note(note)
 	
 			# Sleep a bit
 			time.sleep(0.0001)
@@ -592,6 +606,66 @@ class Buzzer(Output):
 		# with a note duration of 0.01sec
 		# and, boom, we have an alarm!
 		self.melody(range(-30,30),0.01)
+
+	def notes(self,notation,speed=0.5):
+		import re
+
+		# Constant of about 1.0594
+		N = pow( 2.0, (1.0/12.0) )
+
+		# Table of notes, no support for flats YET
+		note_key = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+
+		# Split our notation into individual notes
+		notes = notation.split(' ')
+
+		print notes
+
+		# Set up a list for our parsed output
+		parsed = ['N']
+
+		# Step through each note in turn
+		for note in notes:
+
+			# Split out the note and duration components
+			detail = note.split(':')
+			if len(detail) == 2:
+				# We have a note and a duration
+				note = detail[0]
+				dur = int(detail[1])
+			else:
+				# We have just a note, so duration is 1 beat
+				note = detail[0]
+				dur = 1
+
+			# Now try to match an octave
+			octave = re.findall(r'\d+', note)
+
+			# If we can't find one, default to octave 5
+			if len(octave) == 0:
+				octave = 5.0
+			else:
+				note = note.replace(octave[0],'')
+				octave = float(octave[0])
+
+			# If the note is a rest, turn off for that duration
+			if note == 'R':
+				for _ in range(dur):
+					parsed.append(0) # Frequency of 0 ( off )
+			else:
+			# Otherwise, calculate the pitch of the note from A1 at 55Hz
+				note_index = float(note_key.index( note ))
+
+				# Pitch of the note itself is  1.0594 ^ note_index
+				pitch = 55.000 * pow( N, note_index )
+
+				# Then we shift up 2 to the power of the octave index -1
+				pitch = round( pitch  * pow( 2, ( octave - 1 ) ) ,3)
+
+				for _ in range(dur):
+					parsed.append(pitch)
+
+		self.melody(parsed,speed)
 
 	def stop(self):
 		if self._melody != None:
